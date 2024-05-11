058ff5a69a262d169dd42fde27b1289eb061c4d6
ShrDar <np03cs4a220080@heraldcollege.edu.np>
2024-05-11 21:20:17 +0545

    Added: Validaition added in the Organization Profile Page for name, email, password, address changing

--- src/Ankita/OrgProfile.jsx
+++ src/Ankita/OrgProfile.jsx
@@ -1,4 +1,4 @@
-import { doc, onSnapshot, updateDoc } from "firebase/firestore";
+import { collection, doc, onSnapshot, updateDoc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../../config/firebase";
@@ -6,7 +6,10 @@ import changeBtn from '/change.png'
 
 function OrgProfile() {
     const orgId = useParams('orgId').orgId;
+
+    const [orgs, setOrgs] = useState();
     const [orgInfo, setOrgInfo] = useState();
+
     useEffect(() => {
         try {
             const orgRef = doc(db, "organization", orgId);
@@ -15,9 +18,23 @@ function OrgProfile() {
                 setOrgName(doc.data().name)
                 setOrgEmail(doc.data().email)
                 setOrgAddress(doc.data().address)
+                setOrgPassword(doc.data().pass);
             })
             return unsub;
         } catch(err) {
+            console.error(err);
+        }
+    }, [])
+    useEffect(() => {
+        try {
+            const orgCollectionRef = collection(db, 'organization');
+            onSnapshot(orgCollectionRef, (data) => {
+                setOrgs(data.docs.map((doc) => ({
+                    ...doc.data(),
+                    id: doc.id
+                })))
+            })
+        } catch ( err ) {
             console.error(err);
         }
     }, [])
@@ -25,9 +42,15 @@ function OrgProfile() {
     const [orgName, setOrgName] = useState('');
     const [orgAddress, setOrgAddress] = useState('');
     const [orgEmail, setOrgEmail] = useState('');
+    const [orgPassword, setOrgPassword] = useState('');
+    const [isPassVisible, setIsPassVisible] = useState(false);
 
     const letters = /[a-zA-Z]/g;
     const handleNameChange = async () => {
+        if(orgInfo.name === orgName) {
+            alert("Name not Changed (Old name given)")
+            return;
+        }
         if(orgName === "") {
             alert('Empty Field');
             return;
@@ -40,7 +63,6 @@ function OrgProfile() {
         }
         try {
             const nameRef = doc(db, 'organization', orgId);
-            console.log(orgName)
             await updateDoc(nameRef, {name: orgName})
         } catch (err) {
             console.error(err);
@@ -48,6 +70,10 @@ function OrgProfile() {
     }
     
     const handleEmailChange = async() => {
+        if(orgEmail === orgInfo.email) {
+            alert("Email not changed (Old Email Given)");
+            return;
+        }
         if(orgEmail === "") {
             alert("Empty Field");
             return;
@@ -57,6 +83,12 @@ function OrgProfile() {
             alert('Email should contain letters');
             return;
         }
+        const regex = /^([a-z]||[A-Z]||[0-9])+[@]([a-z]||[A-Z])+[.]([a-z]||[A-Z]||[0-9])+[.]*([a-z]||[A-Z]||[0-9])*$/gm; //regex pattern for email validation
+        if(!regex.test(orgEmail)) { //checking if the entered email matches the given regex pattern
+            alert("Invalid Email");
+            return;
+        }
+
         try {
             const emailRef = doc(db, 'organization', orgId);
             await updateDoc(emailRef, {email: orgEmail})
@@ -65,7 +97,37 @@ function OrgProfile() {
         }
     }
 
+    const handlePasswordChange = () => {
+        if(orgPassword === orgInfo.pass) {
+            alert("Password not changed (Old password given)");
+            return;
+        }
+        if(orgPassword === "") {
+            alert("Empty Field");
+            return;
+        }
+        if(orgPassword.length <= 6) {
+            alert("Password Length should be greater than 6");
+        }
+        if(orgPassword.match(letters)) {}
+        else {
+            alert('Password should contain letters');
+            return;
+        }
+
+        try {
+            const passRef = doc(db, 'organization', orgId);
+            updateDoc(passRef, {pass: orgPassword})
+        } catch( err ) {
+            console.error(err);
+        }
+    }
+
     const handleAddressChange = async () => {
+        if(orgAddress === orgInfo.address) {
+            alert("Address not Changed (Old Address Given)");
+            return;
+        }
         if(orgAddress === "") {
             alert('Empty Field');
             return;
@@ -80,6 +142,16 @@ function OrgProfile() {
             await updateDoc(addressRef, {address: orgAddress})
         } catch(err) {
             console.error(err)
+        }
+    }
+
+    const handleChangeAll = async () => {
+        if((orgInfo.name === orgName || orgInfo.email === orgEmail || orgAddress === orgInfo.address)) {
+            alert('Old Name or Email or Address Given');
+        } else {
+            handleNameChange();
+            handleEmailChange();
+            handleAddressChange();
         }
     }
 
@@ -98,7 +170,7 @@ function OrgProfile() {
                     <div className="organizationInfo">
                         <p>Name - {orgInfo.name}</p>
                         <div className="organizationInfoInputChange">
-                            <input type="text" onChange={(e) => setOrgName(e.target.value)} value={orgName} />
+                            <input maxLength={25} type="text" onChange={(e) => setOrgName(e.target.value)} value={orgName} />
                             <div className="changeContainer">
                                 <img src={changeBtn} onClick={() => handleNameChange()} alt="" />
                                 <p>Change Name</p>
@@ -116,16 +188,26 @@ function OrgProfile() {
                         </div>
                     </div>
                     <div className="organizationInfo">
+                        <p>Password -{'>'} {isPassVisible ? orgInfo.pass: '•••••••'}</p>
+                        <div className="organizationInfoInputChange">    
+                            <input type={isPassVisible ? 'text' : 'password'} onChange={(e) => setOrgPassword(e.target.value)} value={orgPassword} onMouseEnter={() => setIsPassVisible(true)} onMouseLeave={() => setIsPassVisible(false)} />
+                            <div className="changeContainer">
+                                <img src={changeBtn} onClick={() => handlePasswordChange()} alt="" onMouseEnter={() => setIsPassVisible(true)} onMouseLeave={() => setIsPassVisible(false)} />
+                                <p>Change Password</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="organizationInfo">
                         <p>Address - {orgInfo.address}</p>
                         <div className="organizationInfoInputChange">    
-                            <input type="text" onChange={(e) => setOrgAddress(e.target.value)} value={orgAddress} />
+                            <input maxLength={30} type="text" onChange={(e) => setOrgAddress(e.target.value)} value={orgAddress} />
                             <div className="changeContainer">
                                 <img src={changeBtn} onClick={() => handleAddressChange()} alt="" />
                                 <p>Change Address</p>
                             </div>
                         </div>
                     </div>
-                    <button className="changeAllBtn">Change All</button>
+                    <button className="changeAllBtn" onClick={() => handleChangeAll()}>Change All</button>
                     
                 </div>
             </div>
